c0dde1d9227fb26cd0186f2dfba908eeec98520e
prajul-shinde <prajulshinde@gmail.com>
2026-08-08 14:33:13 +0530

    fixed nullpointer

--- spring-7-webapp/src/main/java/com/omsai/spring_7_webapp/domain/Author.java
+++ spring-7-webapp/src/main/java/com/omsai/spring_7_webapp/domain/Author.java
@@ -2,6 +2,7 @@ package com.omsai.spring_7_webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
@@ -15,7 +16,7 @@ public class Author {
     private String lastName;
 
     @ManyToMany(mappedBy = "authors")
-    private Set<Book> books;
+    private Set<Book> books = new HashSet<>();
 
     public Set<Book> getBooks() {
         return books;
