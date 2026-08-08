b2c95c378e03de4c38ac1003c2d0354b016e5499
prajul-shinde <prajulshinde@gmail.com>
2026-08-08 12:37:27 +0530

    spring 7 webapp initial commit

---
+++ spring-7-webapp/src/main/java/com/omsai/spring_7_webapp/Spring7WebappApplication.java
@@ -0,0 +1,13 @@
+package com.omsai.spring_7_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring7WebappApplication.class, args);
+	}
+
+}
