27bb865cd1f5ada7ffbd4b430bd573f22b8930a6
prajul-shinde <prajulshinde@gmail.com>
2026-08-09 23:23:16 +0530

    added updatedCustomerById implementation using putmapping

--- spring-7-rest-mvc/src/main/java/com/omsai/spring_7_rest_mvc/service/CustomerServiceImpl.java
+++ spring-7-rest-mvc/src/main/java/com/omsai/spring_7_rest_mvc/service/CustomerServiceImpl.java
@@ -67,4 +67,10 @@ public class CustomerServiceImpl implements CustomerService {
 
         return savedCustomer;
     }
+
+    @Override
+    public void updateCustomerById(UUID customerId, Customer customer) {
+        Customer existing = customerMap.get(customerId);
+        existing.setName(customer.getName());
+    }
 }
