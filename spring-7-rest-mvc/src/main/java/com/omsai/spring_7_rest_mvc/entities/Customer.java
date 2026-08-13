01199cd91376862a5ec86345c0a968953b3a6558
prajul-shinde <prajulshinde@gmail.com>
2026-08-14 03:27:39 +0530

    mysql integration

--- spring-7-rest-mvc/src/main/java/com/omsai/spring_7_rest_mvc/entities/Customer.java
+++ spring-7-rest-mvc/src/main/java/com/omsai/spring_7_rest_mvc/entities/Customer.java
@@ -2,7 +2,9 @@ package com.omsai.spring_7_rest_mvc.entities;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -18,7 +20,8 @@ public class Customer {
     @Id
     @GeneratedValue(generator = "UUID")
     @UuidGenerator
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
     private String name;
 
