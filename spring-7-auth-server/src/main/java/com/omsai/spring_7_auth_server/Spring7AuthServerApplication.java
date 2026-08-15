1452878df614a86b944cbce4482ede709580c556
prajul-shinde <prajulshinde@gmail.com>
2026-08-15 17:41:00 +0530

    spring boot 4 oauthorization server implementation

---
+++ spring-7-auth-server/src/main/java/com/omsai/spring_7_auth_server/Spring7AuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.omsai.spring_7_auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7AuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring7AuthServerApplication.class, args);
+	}
+
+}
---
+++ spring-7-auth-server/src/test/java/com/omsai/spring_7_auth_server/Spring7AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omsai.spring_7_auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7AuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
