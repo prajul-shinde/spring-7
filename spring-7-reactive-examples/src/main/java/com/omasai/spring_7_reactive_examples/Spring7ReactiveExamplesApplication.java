bd726bd4164032a08eab8ac3e254ec9f391cd4c5
prajul-shinde <prajulshinde@gmail.com>
2026-08-16 01:56:38 +0530

    spring boot reactive examples init

---
+++ spring-7-reactive-examples/src/main/java/com/omasai/spring_7_reactive_examples/Spring7ReactiveExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.omasai.spring_7_reactive_examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring7ReactiveExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring7ReactiveExamplesApplication.class, args);
+	}
+
+}
---
+++ spring-7-reactive-examples/src/test/java/com/omasai/spring_7_reactive_examples/Spring7ReactiveExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omasai.spring_7_reactive_examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring7ReactiveExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
