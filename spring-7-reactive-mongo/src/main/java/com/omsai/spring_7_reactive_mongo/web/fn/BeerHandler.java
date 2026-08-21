4429af57f27b5d3907a8dc79c74afa84169b890f
prajul-shinde <prajulshinde@gmail.com>
2026-08-21 18:12:56 +0530

    implemented validation and queryparaams

--- spring-7-reactive-mongo/src/main/java/com/omsai/spring_7_reactive_mongo/web/fn/BeerHandler.java
+++ spring-7-reactive-mongo/src/main/java/com/omsai/spring_7_reactive_mongo/web/fn/BeerHandler.java
@@ -5,10 +5,15 @@ import com.omsai.spring_7_reactive_mongo.services.BeerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.ServerWebInputException;
 import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -16,6 +21,14 @@ import reactor.core.publisher.Mono;
 public class BeerHandler {
 
     private final BeerService beerService;
+    private final Validator validator;
+
+    private void validate(BeerDTO beerDTO) {
+        Errors errors = new BeanPropertyBindingResult(beerDTO, "beerDto");
+        validator.validate(beerDTO, errors);
+        if (errors.hasErrors())
+            throw new ServerWebInputException(errors.toString());
+    }
 
     public Mono<ServerResponse> deleteBeerById(ServerRequest serverRequest) {
         return beerService.getById(serverRequest.pathVariable("beerId"))
@@ -26,6 +39,7 @@ public class BeerHandler {
 
     public Mono<ServerResponse> patchBeerById(ServerRequest serverRequest) {
         return serverRequest.bodyToMono(BeerDTO.class)
+                .doOnNext(this::validate)
                 .flatMap(beerDTO -> beerService.patchBeer(serverRequest.pathVariable("beerId"), beerDTO))
                 .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
                 .flatMap(savedDto -> ServerResponse.noContent().build());
@@ -33,6 +47,7 @@ public class BeerHandler {
 
     public Mono<ServerResponse> updateBeerById(ServerRequest serverRequest) {
         return serverRequest.bodyToMono(BeerDTO.class)
+                .doOnNext(this::validate)
                 .flatMap(beerDto -> beerService.updateBeer(serverRequest.pathVariable("beerId"), beerDto))
                 .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND)))
                 .flatMap(savedDto -> ServerResponse.noContent().build());
@@ -40,6 +55,7 @@ public class BeerHandler {
 
     public Mono<ServerResponse> saveNewBeer(ServerRequest serverRequest) {
         return serverRequest.bodyToMono(BeerDTO.class)
+                .doOnNext(this::validate)
                 .flatMap(beerService::saveBeer)
                 .flatMap(beerDTO -> ServerResponse
                         .created(UriComponentsBuilder.fromUriString(BeerRouterConfig.BEER_PATH_ID).build(beerDTO.getId()))
@@ -54,7 +70,14 @@ public class BeerHandler {
     }
 
     public Mono<ServerResponse> listBeers(ServerRequest serverRequest) {
+
+        Flux<BeerDTO> flux;
+        if (serverRequest.queryParam("beerStyle").isPresent()) {
+            flux = beerService.findByBeerStyle(serverRequest.queryParam("beerStyle").get());
+        } else {
+            flux = beerService.listBeers();
+        }
         return ServerResponse.ok()
-                .body(beerService.listBeers(), BeerDTO.class);
+                .body(flux, BeerDTO.class);
     }
 }
